9972920e147b7edfc8aff3cf892292ba9a129cb1
julietAGuti <juliethaguti@gmail.com>
2018-03-27 21:58:31 -0300

    Add files via upload

---
+++ ClassEjercicio_16/ClassEjercicio_16/Alumno.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassEjercicio_16
+{
+    class Alumno
+    {
+       
+
+        StringBuilder Pantalla = new StringBuilder();
+        static Random Final = new Random();
+
+        public string nombre;
+        public string apellido;
+        public int legajo;
+
+        public float notaFinal = 0;
+        public int nota1 = 0;
+        public int nota2 = 0;
+
+        public void CalcularFinal()
+        {
+           if(nota1 >= 4 && nota2 >= 4)
+            {
+                this.notaFinal = (float)Final.Next(10);
+            }
+           else
+           {
+               this.notaFinal = -1;
+           }
+        }
+        public void Estudiar (int nota1, int nota2)
+        {
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+        public string mostrar()
+        {
+            Pantalla.AppendLine(this.nombre);
+            Pantalla.AppendLine(this.apellido);
+            Pantalla.AppendLine(this.legajo.ToString());
+            Pantalla.AppendLine(this.nota1.ToString());
+            Pantalla.AppendLine(this.nota2.ToString());
+           
+
+            if (this.notaFinal != -1)
+            {
+                Pantalla.AppendLine(this.notaFinal.ToString());
+            }
+            else
+            {
+                Pantalla.AppendLine("Alumno desaprobado");
+            }
+
+            return Pantalla.ToString();
+
+        }
+    }
+}
---
+++ ClassEjercicio_16/ClassEjercicio_16/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEjercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno = new Alumno();
+            Alumno alumno1 = new Alumno();
+            Alumno alumno2 = new Alumno();
+            Alumno alumno3 = new Alumno();
+
+            alumno1.nombre = "Juana";
+            alumno1.apellido = "LaLoca";
+            alumno1.legajo = 105060;
+            alumno1.nota1 = 6;
+            alumno1.nota2 = 7;
+            alumno1.CalcularFinal();
+
+            alumno2.nombre = "Gertrudis";
+            alumno2.apellido = "LaLocomotora";
+            alumno2.legajo = 105061;
+            alumno2.nota1 = 2;
+            alumno2.nota2 = 8;
+            alumno2.CalcularFinal();
+
+            alumno3.nombre = "German";
+            alumno3.apellido = "ElAleman";
+            alumno3.legajo = 105062;
+            alumno3.nota1 = 2;
+            alumno3.nota2 = 2;
+            alumno3.CalcularFinal();
+
+            Console.WriteLine("Datos del alumno1: {0}",alumno1.mostrar());
+            Console.WriteLine("Datos del alumno 2: {0}", alumno2.mostrar());
+            Console.WriteLine("Datos del alumno 3: {0}", alumno3.mostrar());
+            Console.ReadLine();
+
+        }
+    }
+}
