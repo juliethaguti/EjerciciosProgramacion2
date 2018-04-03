3551c9fa6adc365c061e2253822bfcf2cf5046fb
julietAGuti <juliethaguti@gmail.com>
2018-04-03 15:45:21 -0300

    Add files via upload

---
+++ ClassEjercicio_19/ClassEjercicio_19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEjercicio_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador prueba = new Sumador();
+            Console.WriteLine("La suma en numeros es: {0}",prueba.Sumar(22,38));
+            Console.WriteLine("La suma en caracteres es: {0}",prueba.Sumar("Juliet","Andrea"));
+
+            Sumador prueba2 = new Sumador(7);
+            Console.WriteLine("{0}",(int)prueba);
+            Console.WriteLine("{0}",prueba + prueba2);
+            Console.WriteLine("{0}",prueba | prueba2);
+
+            Console.ReadLine();
+        }
+    }
+}
