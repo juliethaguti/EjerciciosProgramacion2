7ff3ef8f6babbd4fb3e0cd840e1d4064393263b7
julietAGuti <juliethaguti@gmail.com>
2018-04-23 21:59:43 -0300

    Add files via upload
    Sin terminar

---
+++ CentralTelefonica/CentralitaHerencia/Centralita.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Centralita
+    {
+        private List<Llamada> listaDeLlamadas;
+        private string razonSocial;
+
+        public Centralita()
+        {
+            listaDeLlamadas = new List<Llamada>();
+        }
+
+        public float GananciaPorLocal 
+        {
+            get
+            {
+                return this.;
+            }
+        }
+        private float CalcularGanancia(TipoLlamada tipo)
+        {
+            
+        }
+    }
+}
---
+++ CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Llamada
+    {
+        protected float _duracion;
+        protected string _nroDestino;
+        protected string _nroOrigen;
+
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+
+        }
+
+        public float Duracion 
+        {
+            get
+            {
+                return this._duracion;
+            }
+        }
+
+        public string NroDestino 
+        {
+            get
+            {
+                return this._nroDestino;
+            }            
+        }
+
+        public string NroOrigen
+        {
+            get
+            {
+                return this._nroOrigen;
+            }
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DURACION: "+this.Duracion.ToString()+" DESTINO: "+this.NroDestino+
+                " ORIGEN: "+this.NroOrigen);
+
+            return sb.ToString();
+        }
+
+        public int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            SortedList<>
+        }
+    }
+}
