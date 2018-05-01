2395b8a3855c3d175cfe9ae4ff2ea93bf0d396e6
julietAGuti <juliethaguti@gmail.com>
2018-05-01 01:17:47 -0300

    Add files via upload
    Terminado

---
+++ ClassEjercicio_23/ClassEjercicio_23/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace ClassEjercicio_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEuroAPesos = new System.Windows.Forms.TextBox();
+            this.txtEuroAEuro = new System.Windows.Forms.TextBox();
+            this.txtPesosAPesos = new System.Windows.Forms.TextBox();
+            this.txtDolarAPesos = new System.Windows.Forms.TextBox();
+            this.txtPesosADolar = new System.Windows.Forms.TextBox();
+            this.txtDolarAEuro = new System.Windows.Forms.TextBox();
+            this.txtPesosAEuros = new System.Windows.Forms.TextBox();
+            this.txtEuroADolar = new System.Windows.Forms.TextBox();
+            this.txtDolarADolar = new System.Windows.Forms.TextBox();
+            this.btnConvertEuro = new System.Windows.Forms.Button();
+            this.btnConvertDolar = new System.Windows.Forms.Button();
+            this.btnConvertPesos = new System.Windows.Forms.Button();
+            this.txtEuro = new System.Windows.Forms.TextBox();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.txtPesos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pesos";
+            // 
+            // txtEuroAPesos
+            // 
+            this.txtEuroAPesos.Location = new System.Drawing.Point(561, 30);
+            this.txtEuroAPesos.Name = "txtEuroAPesos";
+            this.txtEuroAPesos.Size = new System.Drawing.Size(100, 22);
+            this.txtEuroAPesos.TabIndex = 1;
+            // 
+            // txtEuroAEuro
+            // 
+            this.txtEuroAEuro.Location = new System.Drawing.Point(349, 31);
+            this.txtEuroAEuro.Name = "txtEuroAEuro";
+            this.txtEuroAEuro.Size = new System.Drawing.Size(100, 22);
+            this.txtEuroAEuro.TabIndex = 2;
+            // 
+            // txtPesosAPesos
+            // 
+            this.txtPesosAPesos.Location = new System.Drawing.Point(561, 85);
+            this.txtPesosAPesos.Name = "txtPesosAPesos";
+            this.txtPesosAPesos.Size = new System.Drawing.Size(100, 22);
+            this.txtPesosAPesos.TabIndex = 3;
+            // 
+            // txtDolarAPesos
+            // 
+            this.txtDolarAPesos.Location = new System.Drawing.Point(561, 58);
+            this.txtDolarAPesos.Name = "txtDolarAPesos";
+            this.txtDolarAPesos.Size = new System.Drawing.Size(100, 22);
+            this.txtDolarAPesos.TabIndex = 4;
+            // 
+            // txtPesosADolar
+            // 
+            this.txtPesosADolar.Location = new System.Drawing.Point(455, 87);
+            this.txtPesosADolar.Name = "txtPesosADolar";
+            this.txtPesosADolar.Size = new System.Drawing.Size(100, 22);
+            this.txtPesosADolar.TabIndex = 5;
+            // 
+            // txtDolarAEuro
+            // 
+            this.txtDolarAEuro.Location = new System.Drawing.Point(349, 59);
+            this.txtDolarAEuro.Name = "txtDolarAEuro";
+            this.txtDolarAEuro.Size = new System.Drawing.Size(100, 22);
+            this.txtDolarAEuro.TabIndex = 6;
+            // 
+            // txtPesosAEuros
+            // 
+            this.txtPesosAEuros.Location = new System.Drawing.Point(349, 87);
+            this.txtPesosAEuros.Name = "txtPesosAEuros";
+            this.txtPesosAEuros.Size = new System.Drawing.Size(100, 22);
+            this.txtPesosAEuros.TabIndex = 7;
+            // 
+            // txtEuroADolar
+            // 
+            this.txtEuroADolar.Location = new System.Drawing.Point(455, 30);
+            this.txtEuroADolar.Name = "txtEuroADolar";
+            this.txtEuroADolar.Size = new System.Drawing.Size(100, 22);
+            this.txtEuroADolar.TabIndex = 8;
+            // 
+            // txtDolarADolar
+            // 
+            this.txtDolarADolar.Location = new System.Drawing.Point(455, 58);
+            this.txtDolarADolar.Name = "txtDolarADolar";
+            this.txtDolarADolar.Size = new System.Drawing.Size(100, 22);
+            this.txtDolarADolar.TabIndex = 9;
+            // 
+            // btnConvertEuro
+            // 
+            this.btnConvertEuro.Location = new System.Drawing.Point(268, 31);
+            this.btnConvertEuro.Name = "btnConvertEuro";
+            this.btnConvertEuro.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertEuro.TabIndex = 10;
+            this.btnConvertEuro.Text = "->";
+            this.btnConvertEuro.UseVisualStyleBackColor = true;
+            this.btnConvertEuro.Click += new System.EventHandler(this.btnConvertEuro_Click);
+            // 
+            // btnConvertDolar
+            // 
+            this.btnConvertDolar.Location = new System.Drawing.Point(268, 59);
+            this.btnConvertDolar.Name = "btnConvertDolar";
+            this.btnConvertDolar.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertDolar.TabIndex = 11;
+            this.btnConvertDolar.Text = "->";
+            this.btnConvertDolar.UseVisualStyleBackColor = true;
+            this.btnConvertDolar.Click += new System.EventHandler(this.btnConvertDolar_Click);
+            // 
+            // btnConvertPesos
+            // 
+            this.btnConvertPesos.Location = new System.Drawing.Point(268, 87);
+            this.btnConvertPesos.Name = "btnConvertPesos";
+            this.btnConvertPesos.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertPesos.TabIndex = 12;
+            this.btnConvertPesos.Text = "->";
+            this.btnConvertPesos.UseVisualStyleBackColor = true;
+            this.btnConvertPesos.Click += new System.EventHandler(this.btnConvertPesos_Click);
+            // 
+            // txtEuro
+            // 
+            this.txtEuro.Location = new System.Drawing.Point(160, 30);
+            this.txtEuro.Name = "txtEuro";
+            this.txtEuro.Size = new System.Drawing.Size(100, 22);
+            this.txtEuro.TabIndex = 13;
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(160, 58);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(100, 22);
+            this.txtDolar.TabIndex = 14;
+            // 
+            // txtPesos
+            // 
+            this.txtPesos.Location = new System.Drawing.Point(160, 86);
+            this.txtPesos.Name = "txtPesos";
+            this.txtPesos.Size = new System.Drawing.Size(100, 22);
+            this.txtPesos.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Euro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Dolar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Pesos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(479, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Dolar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(378, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 17);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Euro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 121);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPesos);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.txtEuro);
+            this.Controls.Add(this.btnConvertPesos);
+            this.Controls.Add(this.btnConvertDolar);
+            this.Controls.Add(this.btnConvertEuro);
+            this.Controls.Add(this.txtDolarADolar);
+            this.Controls.Add(this.txtEuroADolar);
+            this.Controls.Add(this.txtPesosAEuros);
+            this.Controls.Add(this.txtDolarAEuro);
+            this.Controls.Add(this.txtPesosADolar);
+            this.Controls.Add(this.txtDolarAPesos);
+            this.Controls.Add(this.txtPesosAPesos);
+            this.Controls.Add(this.txtEuroAEuro);
+            this.Controls.Add(this.txtEuroAPesos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEuroAPesos;
+        private System.Windows.Forms.TextBox txtEuroAEuro;
+        private System.Windows.Forms.TextBox txtPesosAPesos;
+        private System.Windows.Forms.TextBox txtDolarAPesos;
+        private System.Windows.Forms.TextBox txtPesosADolar;
+        private System.Windows.Forms.TextBox txtDolarAEuro;
+        private System.Windows.Forms.TextBox txtPesosAEuros;
+        private System.Windows.Forms.TextBox txtEuroADolar;
+        private System.Windows.Forms.TextBox txtDolarADolar;
+        private System.Windows.Forms.Button btnConvertEuro;
+        private System.Windows.Forms.Button btnConvertDolar;
+        private System.Windows.Forms.Button btnConvertPesos;
+        private System.Windows.Forms.TextBox txtEuro;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.TextBox txtPesos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ClassEjercicio_23/ClassEjercicio_23/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Billetes;
+
+namespace ClassEjercicio_23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            estadoInicial();
+
+        }
+
+        private void estadoInicial()
+        {
+            txtEuro.Focus();
+            txtEuroADolar.Enabled = false;
+            txtEuroAEuro.Enabled = false;
+            txtEuroAPesos.Enabled = false;
+            txtDolarAPesos.Enabled = false;
+            txtDolarAEuro.Enabled = false;
+            txtDolarADolar.Enabled = false;
+            txtPesosADolar.Enabled = false;
+            txtPesosAEuros.Enabled = false;
+            txtPesosAPesos.Enabled = false;
+        }
+
+        private void btnConvertEuro_Click(object sender, EventArgs e)
+        {
+            double eu;
+            double.TryParse(txtEuro.Text,out eu);
+            Euro euro = new Euro(eu);
+            txtEuroAEuro.Text = euro.Cantidad.ToString();
+
+            Pesos eurosAPesos = (Pesos)euro;
+             txtEuroAPesos.Text = eurosAPesos.Cantidad.ToString();
+
+             Dolar eurosAdolares = (Dolar)euro;
+             txtEuroADolar.Text = eurosAdolares.Cantidad.ToString();
+        }
+
+        private void btnConvertDolar_Click(object sender, EventArgs e)
+        {
+            double d;
+            double.TryParse(txtDolar.Text,out d);
+            Dolar dolar = new Dolar(d);
+            txtDolarADolar.Text = dolar.Cantidad.ToString();
+
+            Euro euro = (Euro) dolar;
+            txtDolarAEuro.Text = euro.Cantidad.ToString();
+
+            Pesos pesos = (Pesos) dolar;
+            txtDolarAPesos.Text = pesos.Cantidad.ToString();
+        }
+
+        private void btnConvertPesos_Click(object sender, EventArgs e)
+        {
+            double p;
+            double.TryParse(txtPesos.Text, out p);
+            Pesos pesos = new Pesos(p);
+            txtPesosAPesos.Text = pesos.Cantidad.ToString();
+
+            Euro euro = (Euro)pesos;
+            txtPesosAEuros.Text = euro.Cantidad.ToString();
+
+            Dolar dolares = (Dolar)pesos;
+            txtPesosADolar.Text = dolares.Cantidad.ToString();
+        }
+    }
+}
