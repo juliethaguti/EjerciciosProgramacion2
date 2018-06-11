89609b52c9283548c4954132ccded5b8ebe80e5b
julietAGuti <juliethaguti@gmail.com>
2018-06-10 21:32:17 -0300

    Add files via upload

---
+++ ClassEjercicio_46/ClassEjercicio_46/CompetenciaNoDisponibleException.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEjercicio_43
+{
+    public class CompetenciaNoDisponibleException : Exception
+    {
+        private string nombreClase;
+        private string nombreMetodo;
+
+        #region Propiedades
+        public string NombreClase 
+        {
+            get
+            {
+                return this.nombreClase;
+            }
+        }
+
+        public string NombreMetodo
+        {
+            get
+            {
+                return this.nombreMetodo;
+            }
+        }
+
+        #endregion
+
+        #region Constructores
+        public CompetenciaNoDisponibleException(string mensaje, string clase, string metodo)
+            : this(mensaje, clase, mensaje, null)
+        {
+
+        }
+        public CompetenciaNoDisponibleException(string mensaje, string clase, string metodo, Exception innerException)
+            : base(mensaje, innerException)
+        {
+            this.nombreClase = clase;
+            this.nombreMetodo = metodo;
+        }   
+        #endregion
+
+        #region Metodo
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Excepción en el método {0} de la clase {1}:",this.NombreMetodo,this.NombreClase);
+            sb.AppendLine(this.Message);
+            Exception e = this.InnerException;
+            do
+            {
+                sb.AppendLine("\t" + e.Message);
+            } while (!ReferenceEquals(e.InnerException, null));
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ ClassEjercicio_46/UnitTestClassEjercicio_46/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassEjercicio_30;
+using ClassEjercicio_43;
+
+namespace UnitTestClassEjercicio_46
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidarInstanciaListaVehiculos()
+        {
+            Competencia c = new Competencia(10, 20, TipoCompetencia.MotoCross);
+
+            Assert.IsNotNull(c.Competidores);
+        }
+
+        [TestMethod]
+        public void ControlarExcepcionCargarAutoEnMotocrosss()
+        {
+            Competencia motocross = new Competencia(20, 30, TipoCompetencia.MotoCross);
+            AutoF1 auto = new AutoF1(8, "Williams", 50);
+            try
+            {
+                bool retorno = motocross + auto;
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(CompetenciaNoDisponibleException));
+            }
+        }
+
+        [TestMethod]
+        public void ControlarExcepcionCargarMotoEnMotocross()
+        {
+            Competencia motocross = new Competencia(30, 15, TipoCompetencia.MotoCross);
+            VehiculoDeCarrera moto = new Motocross(99, "Suzuki");
+
+            try
+            {
+                bool retorno = motocross + moto;
+            }
+            catch(Exception e)
+            {
+                Assert.IsNotInstanceOfType(e, typeof(CompetenciaNoDisponibleException));
+            }
+        }
+
+        [TestMethod]
+        public void ComprobarVehiculoAgregado()
+        {
+            Competencia c = new Competencia(30, 15, TipoCompetencia.F1);
+            VehiculoDeCarrera a = new AutoF1(10, "McClaren");
+
+            if (c + a)
+            {
+                Assert.IsTrue(c == a);
+            }
+        }
+
+        [TestMethod]
+        public void ComprobarVehiculoQuitado()
+        {
+            Competencia c = new Competencia(30, 15, TipoCompetencia.F1);
+            AutoF1 a = new AutoF1(8,"Renault");
+
+            if(c - a)
+            {
+                Assert.IsTrue(c != a);
+            }
+        }
+
+    }
+}
