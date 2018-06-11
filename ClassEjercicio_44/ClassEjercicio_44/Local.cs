c315f8dfc2a78e747afd9bcc6ae7ab63ea207d77
julietAGuti <juliethaguti@gmail.com>
2018-06-10 21:36:25 -0300

    Add files via upload

---
+++ ClassEjercicio_44/ClassEjercicio_44/Local.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Local : Llamada
+    {
+        protected float _costo;
+
+        #region Propierades
+        public override float CostoLlamada
+        {
+            get
+            {
+                return CalcularCosto();
+            }
+        }
+        #endregion
+
+        #region Constructores
+        public Local(string origen, float duracion, string destino, float costo)
+            : base(duracion, destino, origen)
+        {
+            this._costo = costo;
+        }
+
+        public Local(Llamada llamada, float costo)
+            : this(llamada.NroOrigen, llamada.Duracion, llamada.NroDestino, costo)
+        {
+
+        }
+        #endregion
+
+        #region Metodos
+        private float CalcularCosto()
+        {
+            return base.Duracion * this._costo;
+        }
+
+        protected override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("COSTO: " + this.CostoLlamada);
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Sobrecargas
+        public override bool Equals(object obj)
+        {
+            return obj is Local;
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+        #endregion
+    }
+}
---
+++ ClassEjercicio_44/ClassEjercicio_44/Provincial.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    #region Enumeraciones
+    public enum Franja
+    {
+        Franja_1 = 99,
+        Franja_2 = 125,
+        Franja_3 = 66
+    }
+    #endregion
+    public class Provincial : Llamada
+    {
+        protected Franja _franjaHoraria;
+
+        #region Contructores
+
+        public Provincial(Franja miFranja, Llamada llamada)
+            : this(llamada.NroOrigen, miFranja, llamada.Duracion, llamada.NroDestino)
+        {
+
+        }
+        public Provincial(string origen, Franja miFranja, float duracion, string destino)
+            : base(duracion, destino, origen)
+        {
+            this._franjaHoraria = miFranja;
+        }
+
+        #endregion
+
+        #region Propiedades
+        public override float CostoLlamada
+        {
+            get
+            {
+                return CalcularCosto();
+            }
+        }
+        #endregion
+
+        #region Metodos
+        private float CalcularCosto()
+        {
+            float costo = 0;
+
+            switch (this._franjaHoraria)
+            {
+                case Franja.Franja_1:
+                    costo = base.Duracion * (float)Franja.Franja_1 / 100;
+                    break;
+                case Franja.Franja_2:
+                    costo = base.Duracion * (float)Franja.Franja_2 / 100;
+                    break;
+                case Franja.Franja_3:
+                    costo = base.Duracion * (float)Franja.Franja_3 / 100;
+                    break;
+            }
+            return costo;
+        }
+
+        protected override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.Mostrar());
+            sb.AppendLine("COSTO: " + this.CostoLlamada);
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Provincial;
+        }
+        #endregion
+    }
+}
