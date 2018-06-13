770b2ea8d565a95c4349979fcd3982e4780a7789
julietAGuti <juliethaguti@gmail.com>
2018-06-12 22:13:47 -0300

    Add files via upload

---
+++ ClassEjercicio_69/ClassEjercicio_69/FrmPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassEjercicio_69
+{
+    public partial class FrmPrincipal : Form
+    {
+        frmTestDelegados formTestDelegados;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void testDelegadosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formTestDelegados = new frmTestDelegados();
+            formTestDelegados.Owner = this;
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.IsMdiContainer = true;
+
+        }
+    }
+}
---
+++ ClassEjercicio_69/ClassEjercicio_69/frmTestDelegados.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ClassEjercicio_69
+{
+    partial class frmTestDelegados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtActualizar = new System.Windows.Forms.TextBox();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtActualizar
+            // 
+            this.txtActualizar.Location = new System.Drawing.Point(102, 72);
+            this.txtActualizar.Name = "txtActualizar";
+            this.txtActualizar.Size = new System.Drawing.Size(100, 20);
+            this.txtActualizar.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(102, 134);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(100, 23);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // frmTestDelegados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.txtActualizar);
+            this.Name = "frmTestDelegados";
+            this.Text = "frmTestDelegados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtActualizar;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
