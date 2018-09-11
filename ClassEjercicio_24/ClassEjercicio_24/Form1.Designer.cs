e163adeba742b3522873035218f1749dd8606796
julietAGuti <juliethaguti@gmail.com>
2018-09-11 20:25:31 -0300

    Add files via upload

---
+++ ClassEjercicio_24/ClassEjercicio_24/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace ClassEjercicio_24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.txtKelvin = new System.Windows.Forms.TextBox();
+            this.txtFahrenheitAFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtFahrenheitACelsius = new System.Windows.Forms.TextBox();
+            this.txtCelsiusAFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCelsiusACelsius = new System.Windows.Forms.TextBox();
+            this.txtCelsiusAKelvin = new System.Windows.Forms.TextBox();
+            this.txtKelvinAFarenhit = new System.Windows.Forms.TextBox();
+            this.txtKelvinACelsius = new System.Windows.Forms.TextBox();
+            this.txtKelvinAKelvin = new System.Windows.Forms.TextBox();
+            this.txtFahrenhitAKelvin = new System.Windows.Forms.TextBox();
+            this.btnConvertFahrenheit = new System.Windows.Forms.Button();
+            this.btnConvertCelsius = new System.Windows.Forms.Button();
+            this.btnConvertKelvin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fahrenheit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Celsius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelvin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fahrenheit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(431, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Celsius";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(535, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kelvin";
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Location = new System.Drawing.Point(102, 25);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txtFahrenheit.TabIndex = 6;
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(102, 56);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 7;
+            // 
+            // txtKelvin
+            // 
+            this.txtKelvin.Location = new System.Drawing.Point(102, 84);
+            this.txtKelvin.Name = "txtKelvin";
+            this.txtKelvin.Size = new System.Drawing.Size(100, 20);
+            this.txtKelvin.TabIndex = 8;
+            // 
+            // txtFahrenheitAFahrenheit
+            // 
+            this.txtFahrenheitAFahrenheit.Enabled = false;
+            this.txtFahrenheitAFahrenheit.Location = new System.Drawing.Point(294, 27);
+            this.txtFahrenheitAFahrenheit.Name = "txtFahrenheitAFahrenheit";
+            this.txtFahrenheitAFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txtFahrenheitAFahrenheit.TabIndex = 9;
+            // 
+            // txtFahrenheitACelsius
+            // 
+            this.txtFahrenheitACelsius.Enabled = false;
+            this.txtFahrenheitACelsius.Location = new System.Drawing.Point(400, 27);
+            this.txtFahrenheitACelsius.Name = "txtFahrenheitACelsius";
+            this.txtFahrenheitACelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtFahrenheitACelsius.TabIndex = 10;
+            // 
+            // txtCelsiusAFahrenheit
+            // 
+            this.txtCelsiusAFahrenheit.Enabled = false;
+            this.txtCelsiusAFahrenheit.Location = new System.Drawing.Point(294, 56);
+            this.txtCelsiusAFahrenheit.Name = "txtCelsiusAFahrenheit";
+            this.txtCelsiusAFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsiusAFahrenheit.TabIndex = 12;
+            // 
+            // txtCelsiusACelsius
+            // 
+            this.txtCelsiusACelsius.Enabled = false;
+            this.txtCelsiusACelsius.Location = new System.Drawing.Point(400, 56);
+            this.txtCelsiusACelsius.Name = "txtCelsiusACelsius";
+            this.txtCelsiusACelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsiusACelsius.TabIndex = 13;
+            // 
+            // txtCelsiusAKelvin
+            // 
+            this.txtCelsiusAKelvin.Enabled = false;
+            this.txtCelsiusAKelvin.Location = new System.Drawing.Point(506, 56);
+            this.txtCelsiusAKelvin.Name = "txtCelsiusAKelvin";
+            this.txtCelsiusAKelvin.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsiusAKelvin.TabIndex = 14;
+            // 
+            // txtKelvinAFarenhit
+            // 
+            this.txtKelvinAFarenhit.Enabled = false;
+            this.txtKelvinAFarenhit.Location = new System.Drawing.Point(294, 84);
+            this.txtKelvinAFarenhit.Name = "txtKelvinAFarenhit";
+            this.txtKelvinAFarenhit.Size = new System.Drawing.Size(100, 20);
+            this.txtKelvinAFarenhit.TabIndex = 15;
+            // 
+            // txtKelvinACelsius
+            // 
+            this.txtKelvinACelsius.Enabled = false;
+            this.txtKelvinACelsius.Location = new System.Drawing.Point(400, 84);
+            this.txtKelvinACelsius.Name = "txtKelvinACelsius";
+            this.txtKelvinACelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtKelvinACelsius.TabIndex = 16;
+            // 
+            // txtKelvinAKelvin
+            // 
+            this.txtKelvinAKelvin.Enabled = false;
+            this.txtKelvinAKelvin.Location = new System.Drawing.Point(506, 84);
+            this.txtKelvinAKelvin.Name = "txtKelvinAKelvin";
+            this.txtKelvinAKelvin.Size = new System.Drawing.Size(100, 20);
+            this.txtKelvinAKelvin.TabIndex = 17;
+            // 
+            // txtFahrenhitAKelvin
+            // 
+            this.txtFahrenhitAKelvin.Enabled = false;
+            this.txtFahrenhitAKelvin.Location = new System.Drawing.Point(506, 28);
+            this.txtFahrenhitAKelvin.Name = "txtFahrenhitAKelvin";
+            this.txtFahrenhitAKelvin.Size = new System.Drawing.Size(100, 20);
+            this.txtFahrenhitAKelvin.TabIndex = 18;
+            // 
+            // btnConvertFahrenheit
+            // 
+            this.btnConvertFahrenheit.Location = new System.Drawing.Point(208, 25);
+            this.btnConvertFahrenheit.Name = "btnConvertFahrenheit";
+            this.btnConvertFahrenheit.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertFahrenheit.TabIndex = 20;
+            this.btnConvertFahrenheit.Text = "->";
+            this.btnConvertFahrenheit.UseVisualStyleBackColor = true;
+            this.btnConvertFahrenheit.Click += new System.EventHandler(this.btnConvertFahrenheit_Click);
+            // 
+            // btnConvertCelsius
+            // 
+            this.btnConvertCelsius.Location = new System.Drawing.Point(208, 53);
+            this.btnConvertCelsius.Name = "btnConvertCelsius";
+            this.btnConvertCelsius.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertCelsius.TabIndex = 21;
+            this.btnConvertCelsius.Text = "->";
+            this.btnConvertCelsius.UseVisualStyleBackColor = true;
+            this.btnConvertCelsius.Click += new System.EventHandler(this.btnConvertCelsius_Click);
+            // 
+            // btnConvertKelvin
+            // 
+            this.btnConvertKelvin.Location = new System.Drawing.Point(208, 82);
+            this.btnConvertKelvin.Name = "btnConvertKelvin";
+            this.btnConvertKelvin.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertKelvin.TabIndex = 22;
+            this.btnConvertKelvin.Text = "->";
+            this.btnConvertKelvin.UseVisualStyleBackColor = true;
+            this.btnConvertKelvin.Click += new System.EventHandler(this.btnConvertKelvin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 136);
+            this.Controls.Add(this.btnConvertKelvin);
+            this.Controls.Add(this.btnConvertCelsius);
+            this.Controls.Add(this.btnConvertFahrenheit);
+            this.Controls.Add(this.txtFahrenhitAKelvin);
+            this.Controls.Add(this.txtKelvinAKelvin);
+            this.Controls.Add(this.txtKelvinACelsius);
+            this.Controls.Add(this.txtKelvinAFarenhit);
+            this.Controls.Add(this.txtCelsiusAKelvin);
+            this.Controls.Add(this.txtCelsiusACelsius);
+            this.Controls.Add(this.txtCelsiusAFahrenheit);
+            this.Controls.Add(this.txtFahrenheitACelsius);
+            this.Controls.Add(this.txtFahrenheitAFahrenheit);
+            this.Controls.Add(this.txtKelvin);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Conversor de temperatura";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.TextBox txtKelvin;
+        private System.Windows.Forms.TextBox txtFahrenheitAFahrenheit;
+        private System.Windows.Forms.TextBox txtFahrenheitACelsius;
+        private System.Windows.Forms.TextBox txtCelsiusAFahrenheit;
+        private System.Windows.Forms.TextBox txtCelsiusACelsius;
+        private System.Windows.Forms.TextBox txtCelsiusAKelvin;
+        private System.Windows.Forms.TextBox txtKelvinAFarenhit;
+        private System.Windows.Forms.TextBox txtKelvinACelsius;
+        private System.Windows.Forms.TextBox txtKelvinAKelvin;
+        private System.Windows.Forms.TextBox txtFahrenhitAKelvin;
+        private System.Windows.Forms.Button btnConvertFahrenheit;
+        private System.Windows.Forms.Button btnConvertCelsius;
+        private System.Windows.Forms.Button btnConvertKelvin;
+    }
+}
+
---
+++ ClassEjercicio_24/ClassEjercicio_24/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassEjercicio_21;
+
+namespace ClassEjercicio_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertFahrenheit_Click(object sender, EventArgs e)
+        {
+            double fahrenheit;
+            double.TryParse(this.txtFahrenheit.Text, out fahrenheit);
+            Fahrenheit temperatura = new Fahrenheit(fahrenheit);
+            txtFahrenheitAFahrenheit.Text = temperatura.GetTemperatura().ToString();
+
+            Celsius fahrenheitACelsius = (Celsius)temperatura;
+            this.txtFahrenheitACelsius.Text = fahrenheitACelsius.GetTemperatura().ToString();
+
+            Kelvin fahrenheitAKelvin = (Kelvin)temperatura;
+            this.txtFahrenhitAKelvin.Text = fahrenheitAKelvin.GetTemperatura().ToString();
+        }
+
+        private void btnConvertCelsius_Click(object sender, EventArgs e)
+        {
+            double celsius;
+            double.TryParse(this.txtCelsius.Text, out celsius);
+            Celsius temperatura = new Celsius(celsius);
+            this.txtCelsiusACelsius.Text = temperatura.GetTemperatura().ToString();
+
+            Fahrenheit celsiusAFahrenheit = (Fahrenheit)temperatura;
+            this.txtCelsiusAFahrenheit.Text = celsiusAFahrenheit.GetTemperatura().ToString();
+
+            Kelvin celsiusAKelvin = (Kelvin)temperatura;
+            this.txtCelsiusAKelvin.Text = celsiusAKelvin.GetTemperatura().ToString();
+        }
+
+        private void btnConvertKelvin_Click(object sender, EventArgs e)
+        {
+            double kelvin;
+            double.TryParse(this.txtKelvin.Text, out kelvin);
+            Kelvin temperatura = new Kelvin(kelvin);
+            this.txtKelvinAKelvin.Text = temperatura.GetTemperatura().ToString();
+
+            Fahrenheit celsiusAKelvin = (Fahrenheit)temperatura;
+            this.txtKelvinAFarenhit.Text = celsiusAKelvin.GetTemperatura().ToString();
+
+            Celsius kelvinACelsius = (Celsius)temperatura;
+            this.txtKelvinACelsius.Text = kelvinACelsius.GetTemperatura().ToString();
+        }
+    }
+}
